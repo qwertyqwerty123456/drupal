0878a84640e99d3919dcd8a866f7f03d1c8001ec
QWERTY <>
2022-09-06 18:53:21 +0700

    Изменения

--- web/modules/custom/drupalmodule/src/Plugin/Block/TextBlock.php
+++ web/modules/custom/drupalmodule/src/Plugin/Block/TextBlock.php
@@ -1,87 +1,68 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\bootcampmodule\Plugin\Block\TextBlock.
- */
-
-
-namespace Drupal\bootcampmodule\Plugin\Block;
+namespace Drupal\drupalmodule\Plugin\Block;
 
 use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessResultReasonInterface;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 
 /**
- * Provides a block with header and text editor
+ * Provides a block with a simple text.
  *
  * @Block(
- *   id = "bootcamp_text_block",
- *   admin_label = @Translation("bootcamp_text_block"),
+ *   id = "drupalbook_text_block_block",
+ *   admin_label = @Translation("Textblock"),
  * )
  */
-class TextBlock extends BlockBase
-{
-    /**
-     * Add fields from configuration to page
-     *
-     * {@inheritdoc}
-     */
-    public function build(): array
-    {
-        $config = $this->getConfiguration();
+class TextBlock extends BlockBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
 
-        return [
-            '#text' => $config['text'],
-            '#header' => $config['header']
-        ];
-    }
+    return [
+        '#header' => $config['header'],
+        '#text' => $config['text']
+        
+    ];
+  }
 
-    /**
-     * Access settings
-     *
-     * {@inheritdoc}
-     */
-    protected function blockAccess(AccountInterface $account)
-    {
-        return AccessResult::allowedIfHasPermission($account, 'access content');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  protected function blockAccess(AccountInterface $account) {
+    return AccessResult::allowedIfHasPermission($account, 'access content');
+  }
 
-    /**
-     * Set fields in block settings page
-     *
-     * {@inheritdoc}
-     */
-    public function blockForm($form, FormStateInterface $form_state): array
-    {
-        $config = $this->getConfiguration();
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $config = $this->getConfiguration();
 
-        $form['header'] = [
-            '#type' => 'textfield',
-            '#title' => $this->t('Header'),
-            '#default_value' => $config['header'] ?? '',
-        ];
+    $form['header'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('header'),
+        '#default_value' => $config['header'] ?? '',
+    ];
 
-        $form['text'] = [
-            '#type' => 'text_format',
-            '#allowed_formats' => ['restricted_html' => 'restricted_html'],
-            '#title' => $this->t('Content'),
-            '#default_value' => $config['text']['value'] ?? '',
-        ];
+    $form['text'] = [
+        '#type' => 'text_format',
+        '#allowed_formats' => ['basic_html' => 'basic_html'],
+        '#title' => $this->t('Content'),
+        '#default_value' => isset($config['text']['value']) ? $config['text']['value'] : '',
+    ];
 
-        return $form;
-    }
+    return $form;
+  }
 
-    /**
-     * Action, when user save block configuration
-     *
-     * {@inheritdoc}
-     */
-    public function blockSubmit($form, FormStateInterface $form_state)
-    {
-        $this->configuration['header'] = $form_state->getValue('header');
-        $this->configuration['text'] = $form_state->getValue('text');
-    }
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['header'] = $form_state->getValue('header');
+    $this->configuration['text'] = $form_state->getValue('text');
+  }
 }
